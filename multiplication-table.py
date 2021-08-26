0e054b22ce809699e4087847522f0cf3d3447cbb
Ristani <ristani@gmail.com>
2021-08-26 00:23:21 -0400

    Duplicate encoder

--- multiplication-table.py
+++ multiplication-table.py
@@ -7,18 +7,18 @@ For example, when given size is 3:
 2 4 6
 3 6 9
 
-For given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+For given example, the return value should be: [[1, 2, 3], [2, 4, 6], [3, 6, 9]]
 """
 
 def multiplication_table(size):
     # Create an empty table to return.
     result = []
     # For each row.
-    for r in range(1, size+1):
+    for r in range(1, size + 1):
         #Create an empty table.
         row = []
         # And with each column.
-        for c in range(1, size+1):
+        for c in range(1, size + 1):
             # Multiply and append the results.
             row.append(r * c)
         # Add each row as it's own table of results.
